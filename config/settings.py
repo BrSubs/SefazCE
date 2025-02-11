342fb5692827c84546733eab5788ffd1638b00a5
Yan Brasil <40834307+BrSubs@users.noreply.github.com>
2025-02-11 09:06:08 -0300

    Delete config directory

--- config/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TESSERACT_PATH = os.getenv('TESSERACT_PATH')
-CHROME_DRIVER_PATH = os.getenv('CHROME_DRIVER_PATH')
